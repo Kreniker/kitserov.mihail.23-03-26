ded71edf3f2a6163d15661aed378f98fcb5144c9
Kreniker <kitserov.mike@gmail.com>
2026-03-23 12:27:18 +0300

    test cases explain

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <ios>
 #include <iostream> 
 #include "vector.hpp"
 #include <cstddef>
+#include <utility>
 
 bool test1()
 {
@@ -12,11 +14,15 @@ bool test1()
 int main()
 {
     using test_t = bool(*)();
-    test_t tests[] = {
-        test1
+    using case_t = std::pair<test_t, const char*>;
+    case_t tests[] = {
+        {test1, "Defaul constructor vector must be empty"}
     };
-    size_t count = sizeof(tests) / sizeof(tests[0]);
+    size_t count = sizeof(tests) / sizeof(case_t);
+    std::cout << std::boolalpha;
     for(size_t i = 0; i < count; ++i) {
-        std::cout << tests[i]() << ": " << i << "\n";
+        std::cout << tests[i].first() << ": " << tests[i].second << "\n";
     }
+    return 0;
+    
 }
