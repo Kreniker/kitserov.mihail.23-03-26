eb46c04610be6e07899a2eeb86ed15ce6d3f5e76
Kreniker <kitserov.mike@gmail.com>
2026-03-23 10:41:18 +0300

    operator ==

--- vector.hpp
+++ vector.hpp
@@ -2,13 +2,18 @@
 #define VECTOR_HPP
 
 #include <cstddef>
-namespace identifier {
+namespace topit {
     template < class T >
     struct Vector
     {
       T* data;
       size_t size, capacity;
     };
+    template<class T>
+    bool operator==(Vector<T> v1, Vector<T> v2)
+    {
+        return v1.data == v2.data;
+    }
 }
 
 #endif
