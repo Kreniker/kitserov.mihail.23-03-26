ef2afc63fc474454d7ce8479748aa4c70ae1d100
Kreniker <kitserov.mike@gmail.com>
2026-03-23 10:55:35 +0300

    start testing

--- vector.hpp
+++ vector.hpp
@@ -6,11 +6,15 @@ namespace topit {
     template < class T >
     struct Vector
     {
+      Vector();
+      ~Vector();
+      bool isEmpty() const noexcept;
+    private:
       T* data;
       size_t size, capacity;
     };
     template<class T>
-    bool operator==(Vector<T> v1, Vector<T> v2)
+    bool operator==(const Vector<T>& v1, const Vector<T>& v2)
     {
         return v1.data == v2.data;
     }
