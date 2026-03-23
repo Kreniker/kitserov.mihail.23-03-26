773ed1b48114e475eaf64a5f47e0d9bbb538a281
Kreniker <kitserov.mike@gmail.com>
2026-03-23 13:22:01 +0300

    fix success

--- main.cpp
+++ main.cpp
@@ -25,10 +25,10 @@ bool test3()
 bool test4()
 {
     constexpr size_t size = 3ull;
-    Vector<int> v(size, 0);
+    Vector<int> v(size, 1);
     try {
-        v.at(0);
-        return true;
+        int& value = v.at(0);
+        return value == 1;
     } catch(...) {
         return false;
     }
@@ -49,10 +49,10 @@ bool test5()
 bool test6()
 {
     constexpr size_t size = 3ull;
-    const Vector<int> v(size, 0);
+    const Vector<int> v(size, 1);
     try {
-        v.at(0);
-        return true;
+        const int& value = v.at(0);
+        return value == 1;
     } catch(...) {
         return false;
     }
